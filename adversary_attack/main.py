f2e52d927b6d548df8cf84e16156c3faa966d7a6
Stefanie Kobsar Rodríguez <stkobsar7@gmail.com>
2020-06-25 00:41:05 +0200

    load the images and set the path of the images

--- adversary_attack/main.py
+++ adversary_attack/main.py
@@ -2,13 +2,22 @@ import tensorflow as tf
 import keras
 import matplotlib.pyplot as plt
 import numpy as np
-
-#modelo preentrenado
-
+import os
+from PIL import Image
 from keras.applications.inception_v3 import InceptionV3, decode_predictions
+from keras_preprocessing import image
 from keras import backend as K
 
-iv3 = InceptionV3()
+iv3 = InceptionV3() #Descargar el modelo ya entrenado
+#print(iv3.summary())
 
+absolute_path_every_machine = os.path.abspath(__file__)
+adv_dir = os.path.dirname(os.path.dirname(absolute_path_every_machine))
+adv_dir_data = os.path.join(adv_dir, "data")
+path_image_beer = os.path.join(adv_dir_data, "cerveza.jpeg")
+path_image_cat = os.path.join(adv_dir_data, "gato.jpeg")
+
+image_cat = image.load_img(path_image_cat)
+image_beer = image.load_img(path_image_beer)
 
 
