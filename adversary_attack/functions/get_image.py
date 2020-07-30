67af3cb43eb4094d5c0fc5d792b98c1f12e2ab98
Stefanie Kobsar Rodríguez <stkobsar7@gmail.com>
2020-07-30 18:57:54 +0200

    Build get_image to call it from main

--- adversary_attack/functions/get_image.py
+++ adversary_attack/functions/get_image.py
@@ -6,3 +6,37 @@ InceptionV3 trabaja con rangos de intensidad de -1 a 1, y por tanto hay que rees
 Si se quiere añadir más de una imagen a la red neuronal, habrá que aumentar las dimensiones de la matriz.
 """
 
+import numpy as np
+from keras.applications.inception_v3 import InceptionV3, decode_predictions
+from keras_preprocessing import image
+
+iv3 = InceptionV3() #Descargar el modelo ya entrenado
+#print(iv3.summary())
+
+
+
+def image_to_array(image_path):
+    image_into_array = image.img_to_array(image.load_img(image_path, target_size=(299, 299))) #imagen loded in tensor and reshaped
+    return image_into_array
+
+def normalize_matrix_image(matrix_image):
+    arr = matrix_image - matrix_image.mean(axis=0)
+    matrix_image_norm = arr / np.abs(arr).max(axis=0)
+    return matrix_image_norm
+
+def image_reshaped(image):
+    """
+    Description: reshape to 4 dimension is needed. First dimension (1) can save multiple images in the first dimenson of matrix
+    :param image: image converted in array
+    :return: image reshaped
+    """
+    image_reshaped = image.reshape([1, image.shape[0], image.shape[1], image.shape[2]])
+    return image_reshaped
+
+
+
+def get_image(image_path):
+    image_into_array = image_to_array(image_path)
+    image_norm = normalize_matrix_image(image_into_array)
+    image_norm_reshaped = image_reshaped(image_norm)
+    return image_norm_reshaped
--- adversary_attack/main.py
+++ adversary_attack/main.py
@@ -3,42 +3,34 @@ import keras
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import argparse
+import adversary_attack.functions.get_image as gt
 from PIL import Image
 from keras.applications.inception_v3 import InceptionV3, decode_predictions
 from keras_preprocessing import image
 from keras import backend as K
 
-iv3 = InceptionV3() #Descargar el modelo ya entrenado
-#print(iv3.summary())
 
-absolute_path_every_machine = os.path.abspath(__file__)
-adv_dir = os.path.dirname(os.path.dirname(absolute_path_every_machine))
-path_image_beer = os.path.join(adv_dir, "data/cerveza.jpeg")
-path_image_cat = os.path.join(adv_dir, "data/gato.jpeg")
-
-image_cat = image.img_to_array(image.load_img(path_image_cat, target_size=(299, 299)))
-image_beer = image.img_to_array(image.load_img(path_image_beer, target_size=(299, 299)))
-
-"""
-Image has to be (299, 299, 3) to be interpreted correctly by InceptionV3
-Los valores de la matriz resultante son los valores de la intensidad de cada uno de los pixels en cada uno de sus canales.
-Los valores originales van de 0 a 255. 
-InceptionV3 trabaja con rangos de intensidad de -1 a 1, y por tanto hay que reescalar los valores. 
-Si se quiere añadir más de una imagen a la red neuronal, habrá que aumentar las dimensiones de la matriz.
-"""
-def normalize_matrix_image(matrix_image):
-    arr = matrix_image - matrix_image.mean(axis=0)
-    matrix_image_norm = arr / np.abs(arr).max(axis=0)
-    return matrix_image_norm
-
-image_cat_norm = normalize_matrix_image(image_cat)
-#reshape
-image_cat_reshape = image_cat_norm.reshape([1, image_cat_norm.shape[0], image_cat_norm.shape[1], image_cat_norm.shape[2]])
-print(image_cat_reshape)
-
-#Hasta aqui todo es get image
+def parse_argse():
+    parser = argparse.ArgumentParser(description='get image to procces it in iv3')
+    parser.add_argument('--image_path', type=str, help='image path to process', default="data")
+    args = parser.parse_args()
+    return args.image_path
 
 
+def main(image_path):
+    absolute_path_every_machine = os.path.abspath(__file__)
+    adv_dir = os.path.dirname(os.path.dirname(absolute_path_every_machine))
+    image_relative_path = image_path
+    image_path = os.path.join(adv_dir, image_relative_path)
+    image_processed = gt.get_image(image_path)
+    return image_processed
+
+
+if __name__ == "__main__":
+    image_path = parse_argse()
+    image_processed = main(image_path)
+    print(image_processed)
 
 
 
