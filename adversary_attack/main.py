41a6c46b6a0730b2a67c58cb4ee948d4521fdea1
Stefanie Kobsar Rodríguez <stkobsar7@gmail.com>
2020-09-05 21:41:36 +0200

    trying adv image

--- adversary_attack/main.py
+++ adversary_attack/main.py
@@ -8,10 +8,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import adversary_attack.functions.get_image as gt
 import adversary_attack.functions.get_prediction as gp
-import adversary_attack.functions.adversary_attack as aa
+import adversary_attack.functions.adversary_image as aa
+import adversary_attack.functions.get_class_adv_pred as gc
 from PIL import Image
 from keras_preprocessing import image
 
+def plot_image_adv(image_denormalized):
+    plt.imshow(image_denormalized[0].astype(np.uint8))
+    image = plt.savefig("test_1")
+    return image
 
 def parse_argse():
     parser = argparse.ArgumentParser(description='get image to procces it in iv3')
@@ -39,15 +44,14 @@ if __name__ == "__main__":
     image_processed = main(image_path)
 
     image_pred_copy = aa.adversary(image_processed)
-    #image_denormalized = gt.denormalize_matrix_image(image_pred_copy)
+
+    image_denormalized = gt.denormalize_matrix_image(image_pred_copy)
+    adversary_image = plot_image_adv(image_denormalized)
+
 
 
 
     ########## Show image with moved pixels ###############
-
-    plt.imshow(image_denormalized[0].astype(np.uint8))
-    plt.savefig("test_1")
-
 
 
 
